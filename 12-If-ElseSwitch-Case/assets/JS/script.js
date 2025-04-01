ab4821c488f0f87e8c0304e0e852987121621f1d
Jumshud <soltanovc56@gmail.com>
2025-04-01 23:48:45 +0400

    JavaScript Fuel Station Calc Task && Season Month Script.

--- 12-If-ElseSwitch-Case/assets/JS/script.js
+++ 12-If-ElseSwitch-Case/assets/JS/script.js
@@ -1,10 +1,10 @@
 //! Fuel Calc Task Script
 
-let diesel = "Dizel - 0.90₼";
-let fuel = "Benzin - 1₼";
-let premiumFuel = "Premium Benzin - 1.5₼";
+var diesel = "Dizel - 0.90₼";
+var fuel = "Benzin - 1₼";
+var premiumFuel = "Premium Benzin - 1.5₼";
 
-let fuelType = prompt(`Yanacaq Növü Seçin: 
+var fuelType = prompt(`Yanacaq Növü Seçin: 
 1. ${diesel} 
 2. ${fuel} 
 3. ${premiumFuel}`);
@@ -12,61 +12,62 @@ let fuelType = prompt(`Yanacaq Növü Seçin:
 switch (fuelType) {
   case "1":
     alert(`Seçdiyiniz Yanacaq Növü: ${diesel}`);
-    if (true) {
-      let fuelQuantity = Number(prompt(`Yanacaq Miqdarı Daxil Edin (lt):`));
-      let totalPrice = fuelQuantity * 1;
-      alert("Yekun Mebleğ:" + " " + "₼" + totalPrice);
-      let payment = Number(prompt("Ödeniş ₼: "));
-      if (payment == totalPrice) {
-        alert("Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ 0₼ ");
-      } else if (payment > totalPrice) {
-        let amount = payment - totalPrice;
-        alert(`Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ ${amount}₼ `);
-      } else if (payment < totalPrice) {
-        let amount = payment - totalPrice;
-        alert(`Kifayət Qədər Vəsait Yoxdur: Gərəkli Məbləğ ${amount}₼ `);
-      }
-    }
 
+    var fuelQuantity = Number(prompt(`Yanacaq Miqdarı Daxil Edin (lt):`));
+    var totalPrice = fuelQuantity * 0.9;
+
+    alert("Yekun Mebleğ:" + " " + "₼" + totalPrice);
+    
+    var payment = Number(prompt("Ödeniş ₼: "));
+    if (payment == totalPrice) {
+      alert("Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ 0₼ ");
+    } else if (payment > totalPrice) {
+      var amount = payment - totalPrice;
+      alert(`Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ ${amount}₼ `);
+    } else if (payment < totalPrice) {
+      var amount = payment - totalPrice;
+      alert(`Kifayət Qədər Vəsait Yoxdur: Gərəkli Məbləğ ${amount}₼ `);
+    }
+    
     break;
 
   case "2":
     alert(`Seçdiyiniz Yanacaq Növü: ${fuel}`);
-    if (true) {
-      let fuelQuantity = Number(prompt(`Yanacaq Miqdarı Daxil Edin (lt):`));
-      let totalPrice = fuelQuantity * 0.9;
+
+      var fuelQuantity = Number(prompt(`Yanacaq Miqdarı Daxil Edin (lt):`));
+      var totalPrice = fuelQuantity * 1;
       alert("Yekun Mebleğ:" + " " + "₼" + totalPrice);
-      let payment = Number(prompt("Ödeniş ₼: "));
+      var payment = Number(prompt("Ödeniş ₼: "));
       if (payment == totalPrice) {
         alert("Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ 0₼ ");
       } else if (payment > totalPrice) {
-        let amount = payment - totalPrice;
+        var amount = payment - totalPrice;
         alert(`Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ ${amount}₼ `);
       } else if (payment < totalPrice) {
-        let amount = payment - totalPrice;
+        var amount = payment - totalPrice;
         alert(`Kifayət Qədər Vəsait Yoxdur: Gərəkli Məbləğ ${amount}₼ `);
       }
-    }
+    
 
     break;
 
   case "3":
     alert(`Seçdiyiniz Yanacaq Növü: ${premiumFuel}`);
-    if (true) {
-      let fuelQuantity = Number(prompt(`Yanacaq Miqdarı Daxil Edin (lt):`));
-      let totalPrice = fuelQuantity * 1.5;
+
+      var fuelQuantity = Number(prompt(`Yanacaq Miqdarı Daxil Edin (lt):`));
+      var totalPrice = fuelQuantity * 1.5;
       alert("Yekun Mebleğ:" + " " + "₼" + totalPrice);
-      let payment = Number(prompt("Ödeniş ₼: "));
+      var payment = Number(prompt("Ödeniş ₼: "));
       if (payment == totalPrice) {
         alert("Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ 0₼ ");
       } else if (payment > totalPrice) {
-        let amount = payment - totalPrice;
+        var amount = payment - totalPrice;
         alert(`Ödəniş Uğurla Başa Çatdı Qalığ Məbləğ ${amount}₼ `);
       } else if (payment < totalPrice) {
-        let amount = payment - totalPrice;
+        var amount = payment - totalPrice;
         alert(`Kifayət Qədər Vəsait Yoxdur: Gərəkli Məbləğ ${amount}₼ `);
       }
-    }
+    
 
     break;
 
@@ -76,12 +77,12 @@ switch (fuelType) {
 
 //! Months && Season Script
 
-let autumn = "🌾 Sentyab, 🍁 Oktyab, 🌧 Noyabr";
-let winter = "🌨 Dekabr, ☃️ Yanvar, 🧣 Fevral";
-let summer = "🌸 Mart, 🌿 Aprel, 🌞 May";
-let spring = "🌴 İyun, 🏖 İyul, 🍉 Avqust";
+var autumn = "🌾 Sentyab, 🍁 Oktyab, 🌧 Noyabr";
+var winter = "🌨 Dekabr, ☃️ Yanvar, 🧣 Fevral";
+var summer = "🌸 Mart, 🌿 Aprel, 🌞 May";
+var spring = "🌴 İyun, 🏖 İyul, 🍉 Avqust";
 
-let season = prompt(`Bir Fəsil Seçin:
+var season = prompt(`Bir Fəsil Seçin:
 1. Qış
 2. Yaz
 3. Yay
